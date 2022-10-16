a81b69b9c5a2a4796d91aa133f164c1a2ea72ab6
takyafumin <takyafumin@gmail.com>
2022-10-16 19:34:50 +0900

    プロジェクト一覧画面, ステータス表示機能追加

--- apps/project-management/app/Types/ProjectStatus.php
+++ apps/project-management/app/Types/ProjectStatus.php
@@ -2,6 +2,8 @@
 
 namespace App\Types;
 
+use RuntimeException;
+
 /**
  * Project Status
  */
@@ -24,6 +26,35 @@ enum ProjectStatus: int
             self::DONE    => '対応済',
             self::PENDING => '保留',
             self::CLOSE   => '完了',
+        };
+    }
+
+    /**
+     * ENUM値を生成する
+     *
+     * @param integer $code コード値
+     * @return ProjectStatus
+     */
+    public static function create(int $code): ProjectStatus
+    {
+        return match ($code) {
+            self::NEW->value     => self::NEW,
+            self::WIP->value     => self::WIP,
+            self::DONE->value    => self::DONE,
+            self::PENDING->value => self::PENDING,
+            self::CLOSE->value   => self::CLOSE,
+            default => throw new RuntimeException("定義値がありません, code[{$code}]"),
+        };
+    }
+    public static function iconStyle(int $code): string
+    {
+        return match ($code) {
+            self::NEW->value     => 'bg-blue-200 text-blue-600',
+            self::WIP->value     => 'bg-yellow-200 text-yellow-600',
+            self::DONE->value    => 'bg-green-200 text-green-600 ',
+            self::PENDING->value => 'bg-red-200 text-red-600',
+            self::CLOSE->value   => 'bg-gray-300 text-gray-600',
+            default => throw new RuntimeException("定義値がありません, code[{$code}]"),
         };
     }
 }
